0ff91f17a83e90eb124384abacb6afaff0a1ddf7
Anh D Tran <tran3ah@mail.uc.edu>
2025-11-08 13:05:36 -0500

    Initialing app.js

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import "dotenv/config";
+import connectDB from "./db/db.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Database connection
+await connectDB()
+
+app.use(express.json())
+
+app.get('/', (req, res) => res.send("Server is live..."))
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
