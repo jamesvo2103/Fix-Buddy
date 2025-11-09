93b71b1de2293165c94c8fe4324600c7fd8bb66a
Anh D Tran <tran3ah@mail.uc.edu>
2025-11-08 20:14:01 -0500

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import FixBuddy from './pages/FixBuddy'
+import Login from './pages/Login'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+
+        <Route path='app' element={<Layout />}>
+          <Route index element={<Dashboard />}/>
+          <Route path='/fixbuddy/:fixBuddyId' element={<FixBuddy />}/>
+        </Route>
+
+        <Route path='login' element={<Login />}/>
+
+      </Routes>
+    </>
   )
 }
 
---
+++ frontend/src/components/home/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+        <div className="w-full py-2.5 font-medium text-sm text-purple-800 text-center bg-gradient-to-r from-[#411d96] to-[#FDFEFF]">
+            <p><span className="px-3 py-1 rounded-lg text-white bg-purple-600 mr-2">New</span>AI Feature Added</p>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
