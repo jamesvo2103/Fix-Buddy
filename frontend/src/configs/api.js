dd953223d4573681378be93c14f5c5ce7e84db07
jamesvo2103 <vongochieu2103@gmail.com>
2025-11-09 02:28:30 -0500

    final

--- frontend/src/configs/api.js
+++ frontend/src/configs/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || 'https://fix-buddy.onrender.com'
+    baseURL: import.meta.env.VITE_API_BASE_URL || 'localhost:3001/api',
 })
 
 // Add token to all requests
